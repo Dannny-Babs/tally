cf73e710424778704557e17c4b135f0ca2f066cb
Daniel Babalola <dannybabs123456@gmail.com>
2025-06-11 11:11:09 -0400

    refactor: remove Stats feature exports from utils
    - Deleted exports related to the Stats feature from blocs.dart and screens.dart files to streamline the codebase.
    - This change focuses on maintaining a cleaner project structure by eliminating unused components.

--- lib/utils/screens.dart
+++ lib/utils/screens.dart
@@ -3,7 +3,6 @@ export '../features/transactions/views/expense/add_expense_screen.dart';
 export '../features/transactions/views/income/add_income_screen.dart';
 export '../features/transactions/views/expense/expense_screen.dart';
 export '../features/transactions/views/income/income_screen.dart';
-export '../features/stats/views/stats_screen.dart';
 export '../features/settings/settings_screen.dart';
 export '../features/settings/savings_screen.dart';
 export '../features/settings/edit_profile_screen.dart';
